fc100eab7e6712e709c3cee292e7fbe37487abfc
travisxu <xuzhengwei.amor@gmail.com>
2017-12-14 14:42:27 +0800

    Update about page

--- template/src/components/About.vue
+++ template/src/components/About.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>
-    about
+  <div class="about">
+    <h1>{{msg}}</h1>
+    <h2>Learn more about
+      <a href="https://github.com/koajs-templates/vue.git">koajs-templates/vue</a>.
+    </h2>
+    <router-link to="/">Go to home</router-link>
   </div>
 </template>
 
 <script>
   export default {
     name: 'about',
+    data() {
+      return {
+        msg: 'Welcome to use koajs-templates/vue template.',
+      };
+    },
   };
 </script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
--- template/src/components/HelloWorld.vue
+++ template/src/components/HelloWorld.vue
@@ -9,6 +9,7 @@
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
       <br>
       <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+      <li><router-link to="/about">About</router-link></li>
     </ul>
     <h2>Ecosystem</h2>
     <ul>
@@ -16,7 +17,6 @@
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-      <li><router-link to="/about">about</router-link></li>
     </ul>
   </div>
 </template>
